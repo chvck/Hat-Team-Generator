aa3474dfb16f3ced93744a072c27a1fce115b682
Charles Dixon <chvckd@gmail.com>
2011-12-20 23:27:17 +0000

    Got (almost) everything working upto the point of pushing players to server

--- backend.py
+++ backend.py
@@ -2,14 +2,34 @@ from flask import Flask
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import jsonify
+from werkzeug import secure_filename
+import csv
+
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
 def main():
-    if request.method == 'GET':
-        return redirect(url_for('static', filename='index.html'))
-    elif request.method == 'POST':
-        f = request.files['upload_file']
+    return redirect(url_for('static', filename='index.html'))
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    players = {}
+    data = request.files['inputCSV']
+    if data and allowed_file(data.filename):
+        reader = csv.DictReader(data, delimiter=',')
+        i = 0
+        for row in reader:
+            players[i] = row
+            i += 1
+    players['length'] = len(players);
+    return jsonify(players)   
 
 @app.route('/generate', methods=['POST'])
 def generate():
