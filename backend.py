4693452de8ff0228e2504eb30ea88014f8c86fc4
George Hickman <george@ghickman.co.uk>
2011-12-24 22:36:40 +0000

    Use doc strings for function comments

--- backend.py
+++ backend.py
@@ -42,29 +42,33 @@ def qsort(slist):
     greater = qsort([x for x in slist[1:] if x['points'] <= pivot['points']])
     return lesser + [pivot] + greater
 
-#This splits the players into several lists of the size num_teams, this means that when
-#creating the teams we can pick 1 player from each list for each team
-#TODO: test this with a number of teams not exactly divisible by num_teams
 def split_players(players, num_teams):
+    """
+    This splits the players into several lists of the size num_teams, this means that when
+    creating the teams we can pick 1 player from each list for each team
+    TODO: test this with a number of teams not exactly divisible by num_teams
+    """
     return [players[i*num_teams: (i+1)*num_teams]
             for i in xrange(len(players) // num_teams)]
 
-#Here we create the actual teams. The theory behind this algorithm is that we get the
-#total number of player points available and split the points evenly between each team.
-#We then go over each list of players and randomly assign each player to a team (one per team).
-#The random function takes into account the number of points left available to the current team
-#and weights the random accordingly. If the team has a low number of points left then they get
-#a player that is toward the end of the list (we have ordered the players so that index 0 is the
-#best player and index -1 the worst)
 def teamify(players, num_teams, total_points):
+    """
+    Here we create the actual teams. The theory behind this algorithm is that we get the
+    total number of player points available and split the points evenly between each team.
+    We then go over each list of players and randomly assign each player to a team (one per team).
+    The random function takes into account the number of points left available to the current team
+    and weights the random accordingly. If the team has a low number of points left then they get
+    a player that is toward the end of the list (we have ordered the players so that index 0 is the
+    best player and index -1 the worst)
+    """
     teams = {}
     random.seed()
     points_per_team = total_points // num_teams
-    
-    #create the correct number of teams. Must be a better way to do this? 
+
+    #create the correct number of teams. Must be a better way to do this?
     for i in xrange(num_teams):
         teams[i] = {'players': [], 'points': points_per_team}
-        
+
     for list_players in players:
         if len(list_players) > 0:
             for teamKey in teams:
@@ -116,3 +120,4 @@ def generate():
 
 if __name__ == '__main__':
     app.run(debug = True)
+
